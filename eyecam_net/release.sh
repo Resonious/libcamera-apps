5d05419e2eb9cd91f0f351d52cfdd9c703f93f0f
Nigel Baillie <nigel@baillie.dev>
2023-11-02 14:34:51 +0900

    make release.sh actually work

--- eyecam_net/release.sh
+++ eyecam_net/release.sh
@@ -6,4 +6,5 @@ remote=nigel@192.168.2.102
 
 cross build --release --target aarch64-unknown-linux-gnu
 scp target/aarch64-unknown-linux-gnu/release/libeyecam_net.a "$remote:~/p/libcamera-apps/libs/libeyecam_net.a"
-ssh "$remote" bash -c 'cd ~/p/libcamera-apps/build && meson compile'
+ssh "$remote" bash -c "'cd ~/p/libcamera-apps/build && meson compile'"
+ssh "$remote" sudo systemctl restart eye-server
